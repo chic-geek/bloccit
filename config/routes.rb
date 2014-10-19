960230dccc214393f8fd0fad4ce3f811379fc6ef
Chris Brett <c.brett@icloud.com>
2014-10-19 15:17:08 +0000

    Added posts controller and logic in view templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
-
+  resources :posts
+  get 'about' => 'welcome#about'
   root to: "welcome#index"
-  # could also be written as:
-  # root({to: "welcome#index"}) // example of an implied hash.
 end
