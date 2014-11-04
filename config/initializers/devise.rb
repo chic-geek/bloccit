a9bd8e7a47d54a9079a18ca4de4ab36d12c5346a
Chris Brett <c.brett@icloud.com>
2014-11-04 21:52:42 +0000

    Change devise email add to personal email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'welcome@bloccitapp.com'
+  # config.mailer_sender = 'welcome@bloccitapp.com'
+  config.mailer_sender = 'c.brett@icloud.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
