dc336685dfe6c4d47cfa026e41c6366df6e92e97
Chris Brett <c.brett@icloud.com>
2014-11-05 22:33:41 +0000

    Validating posts, created a new helper method and DRY'd up the forms for adding/editing a post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
     p "Chris Brett"
   end
 
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,15 @@ class Post < ActiveRecord::Base
   belongs_to :topic
 
   default_scope { order('created_at DESC') }
+
+  # Validations are like boucers of a club, they only let in people
+  # that fit the criteria, below we're asking a for the following:
+  # a posts title to be at least 5 chars long and exist,
+  # a posts body to be at least 20 chars long and exist,
+  # a post to be associated with a topic
+  # and a post to be associated with a user.
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
