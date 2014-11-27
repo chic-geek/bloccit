827261e172531144a4e933a1005851fe8b747f7d
Chris Brett <c.brett@icloud.com>
2014-11-27 18:15:40 +0000

    Mocking RSpec, all present and correct

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Post do
+
+  include TestFactories
+
   describe "vote methods" do
 
     before do
@@ -34,29 +37,6 @@ describe Post do
         post.create_vote
         expect(post.up_votes).to eq(1)
       end
-    end
-
-    def associated_post(options={})
-      post_options = {
-        title: 'Post title',
-        body: 'Post bodies must be pretty long',
-        topic: Topic.create(name: 'Topic name'),
-        user: authenticated_user
-      }.merge(options)
-
-      Post.create(post_options)
-    end
-
-    def authenticated_user(options={})
-      user_options = {
-        email: "email#{rand}@fake.com",
-        password: 'password',
-      }.merge(options)
-
-      user = User.new(user_options)
-      user.skip_confirmation!
-      user.save
-      user
     end
 
   end
